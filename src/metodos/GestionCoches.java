23d71f3bbbab13b34b0d45313a03737a48b05a97
SrEstroncio038 <asolagurenanaut@gmail.com>
2026-01-30 10:20:59 +0100

    desgaste

--- src/metodos/GestionCoches.java
+++ src/metodos/GestionCoches.java
@@ -1,7 +1,6 @@
 package metodos;
 
-import java.io.*;
-import java.util.ArrayList;
+
 
 
 import clases.PlanDeCarrera;
@@ -30,19 +29,33 @@ public class GestionCoches {
 		planDeCarrera.setDesgaste(0);
 	}
 	
-	public void repostar(PlanDeCarrera pc) {
+	public void repostar(PlanDeCarrera planDeCarrera) {
 		float gasolina;
 		boolean repostar;
-		System.out.println(String.format("El coche tiene %f litros de gasolina", pc.getLitrosGasolina()));
+		System.out.println(String.format("El coche tiene %f litros de gasolina", planDeCarrera.getLitrosGasolina()));
 		System.out.println("¿Quieres repostar?");
 		repostar= Utilidades.leerBoolean();
 		if(repostar) {
 			System.out.println("¿Cuantos litors de gasolina quieres repostar(Maximo 110L)?");
 			gasolina=Utilidades.leerFloat(0, 110);
-			pc.setLitrosGasolina(gasolina+pc.getLitrosGasolina());
+			planDeCarrera.setLitrosGasolina(gasolina+planDeCarrera.getLitrosGasolina());
 			System.out.println("Se ha repostado con exito.");
 		}
 	}
 
+	public void desgaste(PlanDeCarrera planDeCarrera, float distancia) {
+		float desgaste=0;
+		if(planDeCarrera.getTipoRueda().toString().equalsIgnoreCase("blando")) {
+			desgaste= (float) ((798+planDeCarrera.getLitrosGasolina()*0.7)*9.81*distancia*0.145)/1000;
+		}else if(planDeCarrera.getTipoRueda().toString().equalsIgnoreCase("medio")) {
+			desgaste= (float) ((798+planDeCarrera.getLitrosGasolina()*0.7)*9.81*distancia*0.1)/1000;
+		}else if(planDeCarrera.getTipoRueda().toString().equalsIgnoreCase("duro")) {
+			desgaste= (float) ((798+planDeCarrera.getLitrosGasolina()*0.7)*9.81*distancia*0.075)/1000;
+		}else {
+			desgaste= (float) ((798+planDeCarrera.getLitrosGasolina()*0.7)*9.81*distancia*0.055)/1000;
+		}
+		planDeCarrera.setDesgaste(desgaste);
+	}
+	
 }
 
