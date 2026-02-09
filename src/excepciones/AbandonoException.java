dbe38732db14b5dd8610afbc45262b4f50e3264b
SrEstroncio038 <asolagurenanaut@gmail.com>
2026-02-09 08:10:44 +0100

    añadir id a la excepcion

--- src/excepciones/AbandonoException.java
+++ src/excepciones/AbandonoException.java
@@ -2,6 +2,11 @@ package excepciones;
 
 public class AbandonoException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public AbandonoException(String mensage) {
 		super(mensage);
 	}
